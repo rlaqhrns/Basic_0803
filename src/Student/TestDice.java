0be0d4110bf87014c638c4d75f950cecd588e70f
rlaqhrns93@naver.com <Green_Computer@DESKTOP-912HOCH>
2020-08-03 18:44:21 +0900

    dice 수정

--- src/Student/TestDice.java
+++ src/Student/TestDice.java
@@ -4,11 +4,12 @@ package Student;
 public class TestDice {
 	public static void main(String[] args) {
 		PairOfDice pod = new PairOfDice();
-		Player player = new Player();
+		Player player1 = new Player();
+		Player player2 = new Player();
 		pod.roll();
-		player.setTotal(pod);
+		player1.setTotal(pod);
 
-		System.out.println("선수이름 : " + player.getName() + "\n\t주사위 1: " + pod.getFacevalue1() + "\n\t주사위 2: "
-				+ pod.getFacevalue2() + "\n\t점수: " + player.getTotal());
+		System.out.println("선수이름 : " + player1.getName() + "\n\t주사위 1: " + pod.getFacevalue1() + "\n\t주사위 2: "
+				+ pod.getFacevalue2() + "\n\t점수: " + player1.getTotal());
 	}
 }
